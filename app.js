45a08f9e48ddbe7b582a81e650e6b407052acd0d
Anthony Waweru <anthonypmarcel@gmail.com>
2021-02-26 09:13:36 +0300

    Updated commit

--- app.js
+++ app.js
@@ -41,7 +41,72 @@ UI.prototype.showAlert = function (msgAlert, className) {
   setTimeout(()=> {document.querySelector('.alert').remove()},3000)
 }
 
+//add to local storage
 
+UI.prototype.addToLs = function (book) {
+  let books;
+  if (localStorage.getItem('books') === null) {
+    books = [];
+  } else {
+    books = JSON.parse(localStorage.getItem('books'));  
+  }
+  books.push(book);
+  
+  localStorage.setItem('books', JSON.stringify(books))
+}
+
+//getting and displaying books from the local storage
+UI.prototype.getBook = function () {
+  // const ui = new UI;
+  let books;
+  if (localStorage.getItem('books') === null) {
+    books = [];
+  } else {
+    books = JSON.parse(localStorage.getItem('books'));  
+  }
+
+  books.forEach(function (book) {
+    const ui = new UI
+    ui.addNewBooktolist(book);
+
+     //get the tbody element
+  // const tbody = document.querySelector('.tbody')
+  // //create row element
+  // const row = document.createElement('tr');
+  // //add items from the book to the row innerHTML
+  // row.innerHTML = `
+  // <td>${book.title}</td>
+  // <td>${book.author}</td>
+  // <td>${book.isbn}</td>
+  // <td><a href="#" class="delete">X</a></td>`
+  // tbody.appendChild(row)
+    })
+  localStorage.setItem('books', JSON.stringify(books))
+}
+
+//delete books from local storage
+UI.prototype.removeFromLs = function (e) {
+  const ui = new UI;
+  let books;
+  if (localStorage.getItem('books') === null) {
+    books = [];
+  } else {
+    books = JSON.parse(localStorage.getItem('books'));  
+  }
+  books.forEach(function (book, index) {
+    if (e.target.className === 'delete') {
+      books.splice(index,1);
+    }
+  })
+  localStorage.setItem('books', JSON.stringify(books));
+}
+
+
+//DOM content loade listener
+document.addEventListener('DOMContentLoaded', function (e) {
+  const ui = new UI;
+  ui.getBook();
+})
 //add form event listener and extract from DOM
 document.querySelector('#book-list').addEventListener('submit', function (e) {
   const title = document.querySelector('#book-title').value;
@@ -56,14 +121,12 @@ document.querySelector('#book-list').addEventListener('submit', function (e) {
   } else {
     //add book
     ui.addNewBooktolist(book);
+    ui.addToLs(book);
   // //show success alert when div has been appended to the Dom
    ui.showAlert(`${book.title} by ${book.author} has been added to your book list.`, 'success');
   // //clear the input values
    ui.clearBookField();
-
   }
-
-  
   e.preventDefault();
 })
 
@@ -75,11 +138,11 @@ document.querySelector('.container').addEventListener('click', deleteBook);
 function deleteBook(e) {
   const ui = new UI
   if (e.target.className === 'delete') {
-    e.target.parentElement.parentElement.remove();
-    ui.showAlert('Book deleted from the list.', 'success')
+    e.target.parentElement.parentElement.remove();   
+  ui.showAlert('Book deleted from the list.', 'success');
   }
+  ui.removeFromLs(e);
 }
-
 
 
 
